988e9f1e5c685c51ac0e0044d2bcae27b0f9f5ba
christoph <mail@csc.cx>
2023-02-04 23:37:59 +0100

    remove stuff for wkhtml

--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -18,37 +18,6 @@ class RenderController extends AbstractController
         $data = json_decode($request->getContent());
         if($data && isset($data->content)) {
             $content = $data->content;
-            /* we need to replace the css variables manually since wkhtmltopdf does not support css variables */
-            preg_match_all('/href="styles.(\w+).css"/m', $content, $matches, PREG_SET_ORDER, 0);
-
-            if($matches && isset($matches[0])) {
-                $cssContent = file_get_contents($_SERVER['HTTP_ORIGIN']. '/styles.'. $matches[0][1]. '.css');
-
-                preg_match_all('/var\(--([\w|-]+)\)/m', $cssContent, $variables);
-                $replacements = [];
-                for($i = 0; $i < count($variables[1]); $i++) {
-                    // find value defined in $data->content
-                    preg_match_all('/--'. $variables[1][$i] .':([^;]+);/m', $data->content, $variableValue);
-                    if($variableValue && isset($variableValue[0]) && !empty($variableValue[0])) {
-                        $replacements[$variables[0][$i]] = $variableValue[1][0];
-                    } else {
-                        switch($variables[1][$i]) {
-                            case 'text-color': $replacements[$variables[0][$i]] = '#fff'; break;
-                            case 'border-color': $replacements[$variables[0][$i]] = '#222'; break;
-                            case 'base-size': $replacements[$variables[0][$i]] = '48px'; break;
-                            case 'bg-color': $replacements[$variables[0][$i]] = 'rgba(255,0,0,0.3)'; break;
-                        }
-                    }
-                }
-
-                foreach($replacements as $var => $value) {
-                    $cssContent = str_replace($var, $value, $cssContent);
-                }
-
-                /* replace stylesheet */
-                $content = str_replace('<link rel="stylesheet" href="styles.'. $matches[0][1] .'.css">', '<style>'. $cssContent. '</style>', $content);
-
-            }
             $content = str_replace('background-image: url(', 'background-image: url('. $this->getParameter('frontend_url'), $content);
             $content = str_replace('background-image: url("', 'background-image: url("'. $this->getParameter('frontend_url'), $content);
             $content = str_replace('src: url("', 'src: url("'. $this->getParameter('frontend_url'), $content);
